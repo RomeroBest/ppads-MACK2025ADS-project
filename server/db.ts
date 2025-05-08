ca81cf0216b14b36b08ef24e11f583524c508e16
RomeroBest <155656693+RomeroBest@users.noreply.github.com>
2025-05-08 14:01:52 -0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,13 +1,17 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import { drizzle } from "drizzle-orm/pg-core";
-import { Pool } from "pg";
+import pkg from "pg";
+const { Pool } = pkg;
+
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "../shared/schema";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }, // se precisar no Render
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const db = drizzle(pool, { schema });
